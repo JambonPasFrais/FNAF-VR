5c0aa9c5da91c5d3d680a7da90d5845cf8a2f24e
Remy <remy.jabbo@gmail.com>
2023-01-26 16:36:00 +0100

    audio try

--- FNAF/Assets/Scripts/GameManager.cs
+++ FNAF/Assets/Scripts/GameManager.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public AudioClip Clip;
+    public AudioSource Source;
+    
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Source.clip = Clip;
+        Source.Play();
     }
 }
