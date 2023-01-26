6785227c9b42b05b8e6e14de03cff9beca58d6d9
Remy <remy.jabbo@gmail.com>
2023-01-26 16:59:32 +0100

    add sounds

--- FNAF/Assets/Scripts/GameManager.cs
+++ FNAF/Assets/Scripts/GameManager.cs
@@ -6,10 +6,19 @@ public class GameManager : MonoBehaviour
 {
     public AudioClip Clip;
     public AudioSource Source;
+    System.Random rand = new System.Random();
     
     void Start()
     {
         Source.clip = Clip;
+        StartCoroutine("PlaySound", rand.Next(3, 10));
+    }
+
+    IEnumerator PlaySound(int time)
+    {
+        yield return new WaitForSeconds(time);
+        Source.clip = Clip;
         Source.Play();
+        StartCoroutine("PlaySound", rand.Next(3, 10));
     }
 }
