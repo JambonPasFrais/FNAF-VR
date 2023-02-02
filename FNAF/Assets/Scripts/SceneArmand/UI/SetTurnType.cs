de979e9c1b0ec29746db9a180fb76be1127515f6
Jambon <armand.deffrennes@student.junia.com>
2023-02-02 14:39:41 +0100

    TMP & Menu
    Trying changing between continuous and snap turn

---
+++ FNAF/Assets/Scripts/SceneArmand/UI/SetTurnType.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class SetTurnType : MonoBehaviour
+{
+    public ActionBasedSnapTurnProvider SnapTurn;
+    public ActionBasedContinuousTurnProvider ContinuousTurn;
+
+    public void SetTypeFromIndex(int index)
+    {
+        if (index == 0)
+        {
+            SnapTurn.enabled = false;
+            ContinuousTurn.enabled = true;
+        }
+        else if (index == 1)
+        {
+            SnapTurn.enabled = true;
+            ContinuousTurn.enabled = false;
+        }
+    }
+}
