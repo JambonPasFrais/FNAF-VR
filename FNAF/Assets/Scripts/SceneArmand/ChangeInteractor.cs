39d5e83a541630ddfa50d29c14952f9438257161
Jambon <armand.deffrennes@student.junia.com>
2023-02-02 15:28:48 +0100

    gitignore + UI update

---
+++ FNAF/Assets/Scripts/SceneArmand/ChangeInteractor.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeInteractor : MonoBehaviour
+{
+    public GameObject DirectController;
+    public GameObject RayController;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        DirectController.SetActive(true);
+        RayController.SetActive(false);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        DirectController.SetActive(false);
+        RayController.SetActive(true);
+    }
+}
