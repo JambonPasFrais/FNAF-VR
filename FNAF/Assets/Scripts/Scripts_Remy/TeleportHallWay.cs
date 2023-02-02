475c83de0833e7e268fae13299a1069b85c7140e
Remy <remy.jabbo@gmail.com>
2023-02-02 16:41:54 +0100

    infinite corridor

--- FNAF/Assets/Scripts/Scripts_Remy/TeleportHallWay.cs
+++ FNAF/Assets/Scripts/Scripts_Remy/TeleportHallWay.cs
@@ -8,23 +8,15 @@ public class TeleportHallWay : MonoBehaviour
 
     public Transform HTPPoint;  //Hallway Teleportation Point
 
-    public GameObject BackWalls;
-    public GameObject FrontWalls;
-
     float rotation = 0;
 
 
     private void OnTriggerEnter(Collider other)
     {
-        BackWalls.SetActive(true);
-
         Hallway.transform.position = HTPPoint.position;
 
         rotation += 90;
 
         Hallway.transform.rotation = Quaternion.Euler(0, rotation, 0);
-
-        if(FrontWalls != null)
-            FrontWalls.SetActive(false);
     }
 }
