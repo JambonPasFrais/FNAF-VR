7c21d340398ed68d1ffe36be1a9d34eb0b525fc2
Remy <remy.jabbo@gmail.com>
2023-02-02 15:28:50 +0100

    on avance... on avance...

--- FNAF/Assets/Scripts/Scripts_Remy/TeleportHallWay.cs
+++ FNAF/Assets/Scripts/Scripts_Remy/TeleportHallWay.cs
@@ -6,12 +6,15 @@ public class TeleportHallWay : MonoBehaviour
 {
     public GameObject Hallway;
     public GameObject TransitionHallway;
+
     public Transform THTPPoint; //Transition Hallway Teleportation Point
     public Transform HTPPoint;  //Hallway Teleportation Point
 
+    public GameObject BackWalls;
+    public GameObject FrontWalls;
 
-    // Update is called once per frame
-    void Update()
+
+    private void OnTriggerEnter(Collider other)
     {
         
     }
