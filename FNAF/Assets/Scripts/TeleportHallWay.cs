9d18ef71a48bed7edc25d17279dc817c2539d587
Remy <remy.jabbo@gmail.com>
2023-02-02 15:06:36 +0100

    corridor

---
+++ FNAF/Assets/Scripts/TeleportHallWay.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportHallWay : MonoBehaviour
+{
+    public GameObject Hallway;
+    public GameObject TransitionHallway;
+    public Transform THTPPoint; //Transition Hallway Teleportation Point
+    public Transform HTPPoint;  //Hallway Teleportation Point
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
