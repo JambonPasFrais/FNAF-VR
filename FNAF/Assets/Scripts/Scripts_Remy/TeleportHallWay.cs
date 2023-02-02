969c98e99d4d3bb193903daffab60feb7f14bcfc
Remy <remy.jabbo@gmail.com>
2023-02-02 16:03:05 +0100

    tp hallway v1

--- FNAF/Assets/Scripts/Scripts_Remy/TeleportHallWay.cs
+++ FNAF/Assets/Scripts/Scripts_Remy/TeleportHallWay.cs
@@ -5,17 +5,25 @@ using UnityEngine;
 public class TeleportHallWay : MonoBehaviour
 {
     public GameObject Hallway;
-    public GameObject TransitionHallway;
 
-    public Transform THTPPoint; //Transition Hallway Teleportation Point
     public Transform HTPPoint;  //Hallway Teleportation Point
 
     public GameObject BackWalls;
     public GameObject FrontWalls;
 
+    float rotation = 0;
+
 
     private void OnTriggerEnter(Collider other)
     {
-        
+        BackWalls.SetActive(true);
+
+        Hallway.transform.position = HTPPoint.position;
+
+        rotation += 90;
+
+        Hallway.transform.rotation = Quaternion.Euler(0, rotation, 0);
+
+        FrontWalls.SetActive(false);
     }
 }
