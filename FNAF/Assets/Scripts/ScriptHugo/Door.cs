a825eb1bb01a049be5d700a68b3c055a45134e02
Hugo Vanderhaegen <hugo.vanderhaegen@student.junia.com>
2023-02-09 15:47:57 +0100

    door opening sound

--- FNAF/Assets/Scripts/ScriptHugo/Door.cs
+++ FNAF/Assets/Scripts/ScriptHugo/Door.cs
@@ -1,22 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class Door : MonoBehaviour
 {
     [SerializeField] private AudioSource[] _doorShutSound;
+    [SerializeField] private AudioSource[] _doorMovingSound;
     private bool _isShutting = false;
-    private float _shutForce = 0;
-    public void ShutDoor(float force)
+    public float force;
+    public void ShutDoor()
     {
         _isShutting= true;
         _doorShutSound[Random.Range(0, 2)].Play();
-        _shutForce= force;
+        GetComponent<XRGrabInteractable>().enabled = false;
     }
 
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            Debug.Log("shut");
+            ShutDoor();
+        }
+        if(GetComponent<Rigidbody>().velocity != new Vector3(0, 0, 0))
+        {
+            _doorMovingSound[Random.Range(0, 2)].Play();
+        }
+        
+    }
     private void FixedUpdate()
     {
-        if(_isShutting) this.gameObject.GetComponent<Rigidbody>().AddForce(transform.up*_shutForce, ForceMode.Impulse);
+        if(_isShutting) this.gameObject.GetComponent<Rigidbody>().AddForce(transform.up*force, ForceMode.Impulse);
         _isShutting = false;
     }
 }
