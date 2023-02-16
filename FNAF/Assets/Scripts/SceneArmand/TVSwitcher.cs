87eacc991fb1024586ec093048e77872a72c2d1f
Jambon <armand.deffrennes@student.junia.com>
2023-02-16 16:54:07 +0100

    TV Switch on Finished
    Testing it on VR

---
+++ FNAF/Assets/Scripts/SceneArmand/TVSwitcher.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TVSwitcher : MonoBehaviour
+{
+    public GameObject TVLight;
+    public AudioClip SoundEffect;
+    
+    private SoundManager _soundManager;
+
+    private void Start()
+    {
+        _soundManager = new SoundManager();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        _soundManager.LoopAudioClip(SoundEffect);
+        TVLight.SetActive(true);
+    }
+}
